7d2fdeefda519d3d40e9991162dfb7e89c01f45f
amkkun <amkkun@gmail.com>
2013-05-03 14:14:48 +0900

    Add SchemeT type

--- src/Types.hs
+++ src/Types.hs
@@ -1,4 +1,55 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving
+  , TypeFamilies
+  , FlexibleInstances
+  , MultiParamTypeClasses
+  , UndecidableInstances
+  #-}
+
 module Types where
+
+import Control.Applicative (Applicative)
+import Control.Monad.Base (MonadBase)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.State (StateT (..), MonadState)
+import Control.Monad.Trans.Class (MonadTrans, lift)
+import Control.Monad.Trans.Control
+    ( MonadBaseControl (..)
+    , MonadTransControl (..)
+    , ComposeSt
+    , defaultLiftBaseWith
+    , defaultRestoreM
+    , defaultLiftWith
+    , defaultRestoreT
+    )
+import Data.Map (Map)
+
+data Env
+    = Global (Map Ident Value)
+    | Extended (Map Ident Value) Env
+
+newtype SchemeT m a = SchemeT
+    { runSchemeT :: StateT Env m a
+    } deriving
+    ( Functor
+    , Applicative
+    , Monad
+    , MonadIO
+    , MonadState Env
+    , MonadBase base
+    )
+
+instance MonadTrans SchemeT where
+    lift = SchemeT . lift
+
+instance MonadTransControl SchemeT where
+    newtype StT SchemeT a = StScheme { unStScheme :: StT (StateT Env) a }
+    liftWith = defaultLiftWith SchemeT runSchemeT StScheme
+    restoreT = defaultRestoreT SchemeT unStScheme
+
+instance MonadBaseControl base m => MonadBaseControl base (SchemeT m) where
+    newtype StM (SchemeT m) a = StMSchemeT { unStMSchemeT :: ComposeSt SchemeT m a }
+    liftBaseWith = defaultLiftBaseWith StMSchemeT
+    restoreM = defaultRestoreM unStMSchemeT
 
 data Value
     = Bool Bool
