cd47d1727ea99e35bfb09c93520493c2b5144acf
amkkun <amkkun@gmail.com>
2013-05-09 20:39:14 +0900

    Add define

--- src/Env.hs
+++ src/Env.hs
@@ -2,9 +2,10 @@
 
 module Env where
 
+import Control.Applicative ((<$>))
 import Control.Exception.Lifted (throwIO)
 import Control.Monad.Base (MonadBase)
-import Data.IORef.Lifted (readIORef)
+import Data.IORef.Lifted (readIORef, modifyIORef)
 import qualified Data.Map as Map
 
 import Types
@@ -19,3 +20,28 @@ lookupEnv ref ident = do
         Extended m ref' -> case Map.lookup ident m of
             Nothing -> lookupEnv ref' ident
             Just v -> return v
+
+define :: MonadBase IO m => EnvRef -> Ident -> Value -> SchemeT m Value
+define ref ident val = do
+    modifyIORef ref (insert ident val)
+    return $ Ident ident
+
+insert :: Ident -> Value -> Env -> Env
+insert ident val (Global m) = Global $ Map.insert ident val m
+insert ident val (Extended m r) = Extended (Map.insert ident val m) r
+
+splitIdents :: MonadBase IO m => List Value -> SchemeT m (Ident, List Ident)
+splitIdents (ProperList []) = throwIO $ Undefined "syntax error"
+splitIdents (ProperList idents) = do
+    name:params <- extractIdents idents
+    return (name, ProperList params)
+splitIdents (DottedList [] _) = throwIO $ Undefined "syntax error"
+splitIdents (DottedList idents param) = do
+    param':[] <- extractIdents [param]
+    name:params <- extractIdents idents
+    return (name, DottedList params param')
+
+extractIdents :: MonadBase IO m => [Value] -> SchemeT m [Ident]
+extractIdents [] = return []
+extractIdents ((Ident s):vals) = (s:) <$> extractIdents vals
+extractIdents (s:_) = throwIO $ Undefined $ show s
--- src/Eval.hs
+++ src/Eval.hs
@@ -21,5 +21,11 @@ eval env (Ident i) = do
 evalList :: (MonadBase IO m) => EnvRef -> List Value -> SchemeT m Value
 evalList _ (ProperList ((Ident "quote"):[v])) = return v
 evalList _ (ProperList ((Ident "exit"):_)) = throwIO Exit
+evalList env (ProperList ((Ident "define"):(Ident id'):[v])) = do
+    v' <- eval env v
+    define env id' v'
+evalList env (ProperList ((Ident "define"):(List ids):body)) = do
+    (ident, params) <- splitIdents ids
+    define env ident $ Func (Lambda params body env)
 evalList _ xs@(ProperList _) = throwIO $ Undefined $ show xs
 evalList _ xs@(DottedList _ _) = throwIO $ Undefined $ show xs
