031ab99f33692e2614c1c9e3f82f8b22b26b0ec9
amkkun <amkkun@gmail.com>
2013-05-24 09:14:44 +0900

    Rename preload to lib

--- test/Test/Util.hs
+++ test/Test/Util.hs
@@ -15,7 +15,7 @@ loadTest :: Spec
 loadTest = do
     describe "load" $ do
         it "works" $ do
-            testScheme "(load \"preload/util.scm\") (list 1 2 3)" `shouldReturn`
+            testScheme "(load \"lib/util.scm\") (list 1 2 3)" `shouldReturn`
                 [ Const $ Bool True
                 , Apply (Const $ Number 1) [Const $ Number 2, Const $ Number 3]
                 ]
