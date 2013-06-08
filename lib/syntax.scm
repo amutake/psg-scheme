9b3b397a80327642c61c759eb2508b762d51ca4c
amkkun <amkkun@gmail.com>
2013-06-08 19:43:57 +0900

    Move utility function

--- lib/syntax.scm
+++ lib/syntax.scm
@@ -1,13 +1,3 @@
-(define cadr (lambda (x) (car (cdr x))))
-(define cdar (lambda (x) (cdr (car x))))
-(define caar (lambda (x) (car (car x))))
-(define cddr (lambda (x) (cdr (cdr x))))
-
-(define (map f xs)
-  (if (null? xs)
-      xs
-      (cons (f (car xs)) (map f (cdr xs)))))
-
 (define-macro let
   (lambda (args . body)
     `((lambda ,(map car args) ,@body) ,@(map cadr args))))
--- lib/util.scm
+++ lib/util.scm
@@ -1,1 +1,20 @@
 (define (list . xs) xs)
+
+(define cadr (lambda (x) (car (cdr x))))
+(define cdar (lambda (x) (cdr (car x))))
+(define caar (lambda (x) (car (car x))))
+(define cddr (lambda (x) (cdr (cdr x))))
+
+(define (append xs ys)
+  (if (null? xs)
+      ys
+      (cons (car xs) (append (cdr xs) ys))))
+
+(define (not x) (if x #f #t))
+
+(define (null? x) (if (eqv? x '()) #t #f))
+
+(define (map f xs)
+  (if (null? xs)
+      xs
+      (cons (f (car xs)) (map f (cdr xs)))))
