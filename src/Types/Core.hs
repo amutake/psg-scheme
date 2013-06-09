4cb76c1b20c4e03e82d41eb884e9d083939e9cb5
amkkun <amkkun@gmail.com>
2013-06-09 20:03:54 +0900

    Add MonadBaseControl and MonadTransControl to SchemeT

--- src/Types/Core.hs
+++ src/Types/Core.hs
@@ -1,15 +1,27 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving
   , ConstraintKinds
+  , TypeFamilies
+  , FlexibleInstances
+  , MultiParamTypeClasses
+  , UndecidableInstances
   #-}
 
 module Types.Core where
 
 import Control.Applicative (Applicative)
+import Control.Monad (liftM)
 import Control.Monad.Base (MonadBase)
-import Control.Monad.Error (MonadError, ErrorT)
+import Control.Monad.Error (MonadError, ErrorT (..))
 import Control.Monad.IO.Class (MonadIO)
-import Control.Monad.State(StateT, MonadState)
+import Control.Monad.State(StateT (..), MonadState)
 import Control.Monad.Trans.Class (MonadTrans, lift)
+import Control.Monad.Trans.Control
+    ( MonadBaseControl (..)
+    , MonadTransControl (..)
+    , ComposeSt
+    , defaultLiftBaseWith
+    , defaultRestoreM
+    )
 
 import Types.Exception
 import Types.Syntax
@@ -29,4 +41,16 @@ newtype SchemeT m a = SchemeT
 instance MonadTrans SchemeT where
     lift = SchemeT . lift . lift
 
-type MonadScheme m = (Functor m, Monad m, MonadIO m, MonadBase IO m)
+instance MonadTransControl SchemeT where
+    newtype StT SchemeT a = StScheme { unStScheme :: (Either SchemeException a, Macro) }
+    liftWith f = SchemeT . ErrorT $ liftM return $ StateT $ \s ->
+        liftM (\x -> (x, s))
+              (f $ \t -> liftM StScheme (runStateT (runErrorT (runSchemeT t)) s))
+    restoreT = SchemeT . ErrorT . StateT . const . liftM unStScheme
+
+instance MonadBaseControl base m => MonadBaseControl base (SchemeT m) where
+    newtype StM (SchemeT m) a = StMSchemeT { unStMSchemeT :: ComposeSt SchemeT m a }
+    liftBaseWith = defaultLiftBaseWith StMSchemeT
+    restoreM = defaultRestoreM unStMSchemeT
+
+type MonadScheme m = (Functor m, Monad m, MonadIO m, MonadBase IO m, MonadBaseControl IO m)
