49f4491f86378beb4bdabb537109051b551cc230
amkkun <amkkun@gmail.com>
2013-06-10 09:31:42 +0900

    Add a test case for shift/reset

--- test/Test/Cont.hs
+++ test/Test/Cont.hs
@@ -22,3 +22,11 @@ callCCTest = do
                 , "(cc 1)"
                 , "(+ (cc 1) 100)"
                 ]) `shouldReturn` ["cc", "()", "9", "5", "5"]
+
+shiftResetTest :: Spec
+shiftResetTest = do
+    describe "shift/reset" $ do
+        it "works with simple input" $ do
+            testScheme "(reset (+ 1 (shift (lambda (k) (k 2))) 3))" `shouldReturn` ["6"]
+            testScheme "(reset (+ 1 (shift (lambda (k) (+ 10 (k 2)))) 3))" `shouldReturn` ["16"]
+            testScheme "(* 2 (reset (+ 1 (shift (lambda (k) (+ 10 (k 2)))) 3)))" `shouldReturn` ["32"]
