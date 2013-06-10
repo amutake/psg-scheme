364584c9381286d23e023b8ae98ba3ed8d497c17
amkkun <amkkun@gmail.com>
2013-06-10 09:35:16 +0900

    Add a test case for shift/reset

--- test/Test/Cont.hs
+++ test/Test/Cont.hs
@@ -30,3 +30,11 @@ shiftResetTest = do
             testScheme "(reset (+ 1 (shift (lambda (k) (k 2))) 3))" `shouldReturn` ["6"]
             testScheme "(reset (+ 1 (shift (lambda (k) (+ 10 (k 2)))) 3))" `shouldReturn` ["16"]
             testScheme "(* 2 (reset (+ 1 (shift (lambda (k) (+ 10 (k 2)))) 3)))" `shouldReturn` ["32"]
+        it "works with saving continuation" $ do
+            testScheme (concat
+                [ "(define cc ())"
+                , "(* 2 (reset (+ 1 (shift (lambda (k) (set! cc k) (+ 10 (k 2)))) 3)))"
+                , "(cc 1)"
+                , "(cc 10)"
+                , "(+ (cc 1) 100)"
+                ]) `shouldReturn` ["cc", "32", "5", "14", "105"]
